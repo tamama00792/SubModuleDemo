0bfc0eaece6a9ec6fd3c1d731c691a10893dcf61
satori <tamama00792@gmail.com>
2021-07-13 21:19:58 +0800

    Initial Commit

---
+++ SubModuleDemo/SubModuleDemo.h
@@ -0,0 +1,18 @@
+//
+//  SubModuleDemo.h
+//  SubModuleDemo
+//
+//  Created by 徐冰 on 2021/7/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SubModuleDemo.
+FOUNDATION_EXPORT double SubModuleDemoVersionNumber;
+
+//! Project version string for SubModuleDemo.
+FOUNDATION_EXPORT const unsigned char SubModuleDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubModuleDemo/PublicHeader.h>
+
+
